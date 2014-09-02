7c008530d7727540aa65420106c125904995d7d5
Juan R Flores <citojems@hotmail.com>
2014-09-01 23:39:54 -0500

    splash :D

--- core/src/org/furygames/screens/SplashScreen.java
+++ core/src/org/furygames/screens/SplashScreen.java
@@ -1,18 +1,39 @@
 package org.furygames.screens;
 
-import org.furygames.timer.ScreenSwitchTask;
 
-import com.badlogic.gdx.utils.Timer;
+import aurelienribon.tweenengine.BaseTween;
+import aurelienribon.tweenengine.Tween;
+import aurelienribon.tweenengine.TweenCallback;
+import aurelienribon.tweenengine.TweenEquations;
+import aurelienribon.tweenengine.TweenManager;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 
 public class SplashScreen extends GenericScreen {
-
+	
+	private TweenManager manager;
+	private SpriteBatch batcher;
+	private Sprite sprite;
+	private Texture  logoTexture;
+	
+	
 	public SplashScreen () {
 
 	}
 
 	@Override
 	public void render(float delta) {
-		System.out.println("Soy splash!");
+		manager.update(delta);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batcher.begin();
+		sprite.draw(batcher);
+		batcher.end();
 	}
 
 	@Override
@@ -22,9 +43,44 @@ public class SplashScreen extends GenericScreen {
 
 	@Override
 	public void show() {
-		/* schedule to show main menu screen after 2 seconds */
-		Timer.schedule(new ScreenSwitchTask(EScreen.MENU), 3f);
-	}
+		
+
+	logoTexture = new Texture(Gdx.files.internal("Fondo-logo2.png")); 
+	sprite = new Sprite(logoTexture );
+	sprite.setColor(1, 1, 1, 0);
+		
+	
+	float width = Gdx.graphics.getWidth();
+	float height = Gdx.graphics.getHeight();
+	
+
+	sprite.setSize(sprite.getWidth(), sprite.getHeight() );
+	sprite.setPosition((width / 2) - (sprite.getWidth() / 2), (height / 2)
+			- (sprite.getHeight() / 2));
+	
+	
+	setupTween();
+  	batcher = new SpriteBatch();
+	
+	
+}
+
+	private void setupTween() {
+	   Tween.registerAccessor(Sprite.class, new SpriteAccessor());
+	   manager = new TweenManager();
+	
+	   TweenCallback cb = new TweenCallback() {
+	       @Override
+	       public void onEvent(int type, BaseTween<?> source) {
+	          
+	       }
+	   };
+
+   Tween.to(sprite, SpriteAccessor.ALPHA, .8f).target(1)
+           .ease(TweenEquations.easeInOutQuad).repeatYoyo(1, .7f)
+           .setCallback(cb).setCallbackTriggers(TweenCallback.COMPLETE)
+           .start(manager);
+}
 
 	@Override
 	public void hide() {

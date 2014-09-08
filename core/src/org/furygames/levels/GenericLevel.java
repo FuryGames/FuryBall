8f6df2a4514d107e607fe245109f19a0f29ef197
Matías Muñoz Espinoza <writkas2@gmail.com>
2014-09-08 02:06:44 -0300

    viendo como hacer lo de los niveles

---
+++ core/src/org/furygames/levels/GenericLevel.java
@@ -0,0 +1,16 @@
+package org.furygames.levels;
+
+import com.badlogic.gdx.physics.box2d.World;
+
+public class GenericLevel implements ILevel {
+	
+	protected double time; // Tiempo del nivel
+
+	@Override
+	public void createLevel(World world) {
+	
+		// por hacer: eliminar cuerpos del nivel
+		
+	}
+	
+}
---
+++ core/src/org/furygames/levels/ILevel.java
@@ -0,0 +1,7 @@
+package org.furygames.levels;
+
+import com.badlogic.gdx.physics.box2d.World;
+
+public interface ILevel {
+	public void createLevel(World world);
+}

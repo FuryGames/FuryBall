b9650c8f2cc76ee6152add5cb3cce77352e2be5c
Matías Muñoz Espinoza <writkas2@gmail.com>
2014-09-08 23:46:03 -0300

    creando niveles

--- core/src/org/furygames/levels/GenericLevel.java
+++ core/src/org/furygames/levels/GenericLevel.java
@@ -2,15 +2,18 @@ package org.furygames.levels;
 
 import com.badlogic.gdx.physics.box2d.World;
 
-public class GenericLevel implements ILevel {
+public abstract class GenericLevel implements ILevel {
 	
 	protected double time; // Tiempo del nivel
 
 	@Override
 	public void createLevel(World world) {
-	
-		// por hacer: eliminar cuerpos del nivel
-		
+			
 	}
 	
+	@Override
+	public void destroyLevel(World world) {
+		// TODO Auto-generated method stub
+		
+	}
 }
---
+++ core/src/org/furygames/levels/Level1.java
@@ -0,0 +1,5 @@
+package org.furygames.levels;
+
+public class Level1 extends GenericLevel {
+
+}
